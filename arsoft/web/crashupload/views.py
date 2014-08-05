c4e791988b4045d7d0294597542524a7f254747f
Andreas Roth <aroth@arsoft-online.com>
2014-08-05 07:49:15 +0200

    allow upload from commandline crashuploader without CSRF protection parse POST request and store dump file locally

--- arsoft/web/crashupload/views.py
+++ arsoft/web/crashupload/views.py
@@ -4,6 +4,7 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from .forms import UploadFileForm
 from django.conf import settings
 import os.path
@@ -43,31 +44,71 @@ def home(request):
         })
     return HttpResponse(t.render(c))
 
+def _store_dump_file(file):
+    ret = False
+    if file:
+        item_name = 'dumpdata/' + file.name
+        if default_storage.exists(item_name):
+            print ('dupliacted upload of %s' % file.name)
+            item_path = default_storage.path(item_name)
+        else:
+            item_path = default_storage.save(item_name, ContentFile(file.read()))
+        ret = True
+    return ret
 
+def _get_remote_addr(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+@csrf_exempt
 def submit(request):
     file = None
     error_message = None
-    try:
-        file = request.FILES['file']
-    except KeyError:
-        error_message = 'No file uploaded.'
-        pass
 
-    filedata = None
-    if file:
-        path = default_storage.save(file.name, ContentFile(file.read()))
-        filedata = os.path.join(settings.MEDIA_ROOT, path)
+    if request.method == 'POST':
+        useragent = request.META['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in request.META else None
+        if useragent:
+            is_terra3d_crashuploader = True if 'terra3d-crashuploader' in useragent else False
+        else:
+            is_terra3d_crashuploader = False
+        remote_addr = _get_remote_addr(request)
+
+        applicationfile = request.POST['applicationfile'] if 'applicationfile' in request.POST else None
+        crashid = request.POST['id'] if 'id' in request.POST else None
+        timestamp = request.POST['timestamp'] if 'timestamp' in request.POST else None
+
+        minidumpfile = request.FILES['minidump'] if 'minidump' in request.FILES else None
+        minidumpreportfile = request.FILES['minidumpreport'] if 'minidumpreport' in request.FILES else None
+        coredumpfile = request.FILES['coredump'] if 'coredump' in request.FILES else None
+        coredumpreportfile = request.FILES['coredumpreport'] if 'coredumpreport' in request.FILES else None
+
+        result = False
+        if _store_dump_file(minidumpfile):
+            result = True
+        if _store_dump_file(minidumpreportfile):
+            result = True
+        if _store_dump_file(coredumpfile):
+            result = True
+        if _store_dump_file(coredumpreportfile):
+            result = True
+
+        if is_terra3d_crashuploader:
+            if result:
+                body = "Upload of crash %s (%s, %s) from %s successful" % (crashid, applicationfile, timestamp, remote_addr)
+            else:
+                body = "Upload of crash %s (%s, %s) from %s failed" % (crashid, applicationfile, timestamp, remote_addr)
+            return HttpResponse(body, content_type="text/plain")
+        else:
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            request.session['error_message'] = error_message
+            request.session['result'] = result_code
+            return HttpResponseRedirect(reverse('arsoft.web.crashupload.views.home'))
     else:
-        result_code = False
-
-    body = "Upload of %s successful. %s files %s" % (str(file), filedata, str(request.FILES))
-
-    return HttpResponse(body, content_type="text/plain")
-
-    # Always return an HttpResponseRedirect after successfully dealing
-    # with POST data. This prevents data from being posted twice if a
-    # user hits the Back button.
-    request.session['filename'] = filename
-    request.session['error_message'] = error_message
-    request.session['result'] = result_code
-    return HttpResponseRedirect(reverse('arsoft.web.crashupload.views.home'))
+        body = "No crash dump data provided."
+        return HttpResponse(body, status=400, content_type="text/plain")
